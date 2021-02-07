3c4248eae3f18086e00b5c3b9bfd7d3192ec3be9
cyberarm <matthewlikesrobots@gmail.com>
2021-02-07 10:53:05 -0600

    Initial Commit: SDL2 compiles and runs basic app

---
+++ app/jni/src/gosu_app.cpp
@@ -0,0 +1,41 @@
+#include <Gosu/Gosu.hpp>
+
+class GameWindow : public Gosu::Window
+{
+#define WIDTH 4
+#define HEIGHT 28
+#define COLOR 0xffffffff
+
+public:
+    GameWindow()
+            : Window(640, 480)
+    {
+        set_caption("Gosu Tutorial Game");
+    }
+
+    void update() override
+    {
+    }
+
+    void draw() override
+    {
+        Gosu::Graphics::draw_rect(0, 0, width(), height(), 0xff555555, 0);
+        Gosu::Graphics::draw_rect(10, 10, width() - 20, height() - 20, 0xffff8800, 0);
+
+        // H
+        Gosu::Graphics::draw_rect(28, 28, WIDTH, HEIGHT, COLOR, 0);
+        Gosu::Graphics::draw_rect(28, 28 + HEIGHT / 2 - WIDTH / 2, HEIGHT / 2, WIDTH, COLOR, 0);
+        Gosu::Graphics::draw_rect(28 + HEIGHT / 2, 28, WIDTH, HEIGHT, COLOR, 0);
+
+        // I
+        Gosu::Graphics::draw_rect(28 + HEIGHT, 28, WIDTH, HEIGHT, COLOR, 0);
+        Gosu::Graphics::draw_rect(28 + HEIGHT - 5, 28, HEIGHT / 2, WIDTH, COLOR, 0);
+        Gosu::Graphics::draw_rect(28 + HEIGHT - 5, 28 + HEIGHT - WIDTH, HEIGHT / 2, WIDTH, COLOR, 0);
+    }
+};
+
+int main()
+{
+    GameWindow window;
+    window.show();
+}
---
+++ app/src/main/java/org/example/app/Game.java
@@ -0,0 +1,5 @@
+package org.example.app;
+import org.libsdl.app.SDLActivity;
+
+public class Game extends SDLActivity {
+}
