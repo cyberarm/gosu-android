ae76a14ade3b0374784b94638e8a69b89ef4ca43
cyberarm <matthewlikesrobots@gmail.com>
2021-02-07 14:38:53 -0600

    Progress towards building gosu app

--- app/jni/src/Android.mk
+++ app/jni/src/Android.mk
@@ -5,13 +5,15 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := main
 
 SDL_PATH := ../SDL2
+GOSU_PATH := ../gosu
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+#LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(GOSU_PATH)/include
 
 # Add your application source files here...
-LOCAL_SRC_FILES := sdl_app.cpp
+LOCAL_SRC_FILES := gosu_app.cpp
 
-LOCAL_SHARED_LIBRARIES := SDL2
+LOCAL_SHARED_LIBRARIES := gosu
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
